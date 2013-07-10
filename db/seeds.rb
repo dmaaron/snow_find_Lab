9471554b0cd5f9e5a76b4a54d67b1c37d95e199e
David Aaron <davidmatthewaaron@gmail.com>
2013-07-10 14:01:16 -0400

    haml updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 Sighting.delete_all
 
-Sighting.create(image: 'http://www.google.com', location: '10 E 21st Street, New York, NY')
-Sighting.create(image: 'http://www.google.com', location: '700 Manhattan Avenue, Brooklyn, NY')
-Sighting.create(image: 'http://www.google.com', location: 'Statue of Liberty')
-Sighting.create(image: 'http://www.google.com', location: 'Eiffel tower')
-Sighting.create(image: 'http://www.google.com', location: 'Sidney, Australia')
-Sighting.create(image: 'http://www.google.com', location: 'Area 51')
-Sighting.create(image: 'http://www.google.com', location: 'Antarctica')
+Sighting.create(remote_image_url: 'http://static2.businessinsider.com/image/51b656e9eab8ea0231000005/edward-snowden-is-in-the-process-of-destroying-any-support-and-sympathy-he-has-built-up.jpg', location: '10 E 21st Street, New York, NY')
+Sighting.create(remote_image_url: 'http://venturebeat.files.wordpress.com/2013/06/snowden-hong-kong.png?w=558&h=9999&crop=0', location: '700 Manhattan Avenue, Brooklyn, NY')
+Sighting.create(remote_image_url: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSYcBnjHmZxM7Aw2i6BabZF49qPVIhNk8OSFtP3FWSjl5t6Mbhl7Q', location: 'Statue of Liberty')
+Sighting.create(remote_image_url: 'http://static1.businessinsider.com/image/51bb4235eab8ea793b00002d/the-petition-to-pardon-edward-snowden-just-passed-100000-signatures-and-now-the-white-house-has-to-respond.jpg', location: 'Eiffel tower')
+Sighting.create(remote_image_url: 'http://onpoint.wbur.org/files/2013/06/0624_snowden-hk-full.jpg', location: 'Sidney, Australia')
+Sighting.create(remote_image_url: 'http://rack.0.mshcdn.com/media/ZgkyMDEzLzA2LzIyLzI1L2Vkd2FyZHNub3dkLmY2ZmQwLmpwZwpwCXRodW1iCTk1MHg1MzQjCmUJanBn/87783ae1/ad6/edward-snowden-petition.jpg', location: 'Area 51')
+Sighting.create(remote_image_url: 'http://www.csmonitor.com/var/ezflow_site/storage/images/media/content/2013/0617/0617-snowden-live-chat/16098549-2-eng-US/0617-snowden-live-chat_full_600.jpg', location: 'Antarctica')
 
